155751db430ad573cdbe23e8b1a28924f0e5a1db
ApoorvSingh <>
2021-01-30 01:58:11 +0530

    added variable in terraform

--- main.tf
+++ main.tf
@@ -4,27 +4,29 @@ provider "aws" {
 #    shared_credentials_file = "/Users/apoorvsingh/Impo/credentials.csv"
 }
 
+variable "server_port" { }
+
 resource "aws_instance" "tf-instance" {
-    instance_type = "t2.micro"
-    ami = "ami-0eb9463ef8c71ad92"
-    security_groups = ["${aws_security_group.tf-instance-sg.name}"]
-    tags = {
-        Name = "terraform-instance"
+    instance_type   = "t2.micro"
+    ami             = "ami-0eb9463ef8c71ad92"
+    security_groups = [aws_security_group.tf-instance-sg.name]
+    tags            = {
+        Name        = "terraform-instance"
     }
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello World" > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
+    user_data       = <<-EOF
+                      #!/bin/bash
+                      echo "Hello World" > index.html
+                      nohup busybox httpd -f -p 8080 &
+                      EOF
 }
 
 resource "aws_security_group" "tf-instance-sg" {
-    name = "terraform-instance-sg"
+    name              = "terraform-instance-sg"
     ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port     = var.server_port
+        to_port       = var.server_port
+        protocol      = "tcp"
+        cidr_blocks   = ["0.0.0.0/0"]
     }
 }
 
